461fd27d5ca8043dca1504c33c01498b8a2ad15a
nzebar <71467820+nzebar@users.noreply.github.com>
2021-01-04 02:57:33 -0500

    Delete repositories.tf

--- Terraform/repositories.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_codecommit_repository" "repo1356" { //Calls resource module to create a repository.
-  repository_name = "repo_TESTING" //Names of the repository
-  description     = "This repository will consist of the code for the testing server." //description of repo
-}
-
-resource "aws_codecommit_repository" "repo2356" { //Calls resource module to create a repository.
-  repository_name = "repo_PRODUCTION" //Names of the repository
-  description     = "This repository will consist of the code for the production server." //description of repo
-}
