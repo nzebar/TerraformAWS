9e5df6a98f63b1613ce39aec7f28714bdf593f77
nzebar <71467820+nzebar@users.noreply.github.com>
2021-02-20 00:18:13 -0500

    Create providers.tf

---
+++ TerraformProject/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+     aws = {
+      source  = "hashicorp/aws" //The source for where to pull terraform modules from
+     # version = "~> 2.0" //Pull all compatible modules above version 2.70 
+  } 
+}
+}
+
+provider "aws" {
+  region                  = "us-east-1" //AWS destination region for terraform modules
+  shared_credentials_file = "/home/ec2-user/.aws/credentials" //location of shared key file for Auth.
+  profile = "ec2-user"//Profile used to provision and destroy resources.
+}
+
+provider "github" {
+  alias = "githubOAuth"
+  owner = "nzebar"
+  token = "44b06c41373f2007784de6ba5254a34ba51d3907"
+}
